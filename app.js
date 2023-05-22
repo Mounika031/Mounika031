52806168ba63a1dfcec6a2210a8f34a66c19f1d4
Mounika031 <91376709+Mounika031@users.noreply.github.com>
2023-05-22 10:52:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,64 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+const validator = require('validator');
+const yargs = require('yargs');
+const chalk = require('chalk').default;
+const notes = require('./notes')
 
-const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
+yargs.version('1.1.0')
+const command = process.argv[2]
+yargs.command({
+    command: "add",
+    describe: "Add Note",
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function (argv) {
+        
+        console.log('title:' + argv.title)
+        console.log('Body:'+argv.body)
+        notes.addNote(argv.title,argv.body)
+        
+    }
+})
+yargs.command({
+    command: "remove",
+    describe: "remove Note",
+    builder:{
+        title:{
+            describe:"Note title",
+            demandOption:true,
+            type:"string"
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title,argv.body)
 
-modalBtn.addEventListener("click", function () {
-  modal.classList.add("open-modal");
-});
-closeBtn.addEventListener("click", function () {
-  modal.classList.remove("open-modal");
-});
+
+        console.log("removing a new note")
+    }
+})
+yargs.command({
+    command: "list",
+    describe: "Listing a note",
+    handler: function () {
+        console.log("Listing a note")
+    }
+})
+yargs.command({
+    command: "read",
+    describe: "Reading a note",
+    handler: function () {
+        const getNotes = require('./notes')
+    }
+
+})
+yargs.parse()
+//console.log(yargs.argv)
---
+++ utils.js
@@ -0,0 +1,7 @@
+console.log("Utils.js")
+const name="Reddy"
+console.log(name)
+const add=function(a,b){
+return a+b;
+}
+module.exports={name:"Mounika Reddy",add:add}
